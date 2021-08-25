fb3b57f2c2adc8bd3fe5fd329203f1834703ae4a
Mahima Goyal <mahimaagrawal125@gmail.com>
2021-08-25 21:40:48 +0530

    API files

--- vlearnv1/src/main/java/com/target/vlearnv1/repository/UpdateProgress.java
+++ vlearnv1/src/main/java/com/target/vlearnv1/repository/UpdateProgress.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.target.vlearnv1.repository;
 
 import com.target.vlearnv1.entity.SchCompletion;
@@ -11,17 +10,3 @@ import org.springframework.stereotype.Repository;
 public interface UpdateProgress extends CrudRepository<SchCompletion,Integer> {
 
 }
-=======
-package com.target.vlearnv1.repository;
-
-import com.target.vlearnv1.entity.SchCompletion;
-import com.target.vlearnv1.entity.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UpdateProgress extends CrudRepository<SchCompletion,Integer> {
-
-}
->>>>>>> faf423c9ce3092d2de2445f77e0336be5112291d
